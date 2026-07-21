f4b695c90d1cdae737738084017a15252dfb0bc6
callum128 <callum.rc9@gmail.com>
2026-07-22 11:17:29 +1200

    More scans

--- lifetimes_toml_read.py
+++ lifetimes_toml_read.py
@@ -8,37 +8,17 @@ def single_exponential(t, A, tau, y0):
     return A * np.exp(-t / tau) + y0
 
 def double_exponential(t, A1, tau1, A2, tau2, y0):
-    return A1 * np.exp(-t / tau1) + A2 * np.exp(-t / tau2) + y
+    return A1 * np.exp(-t / tau1) + A2 * np.exp(-t / tau2) + y0
 
-odd_1D2_3H4_lifetime = 'Outputs/(3P0)1D2-3H4_488.35nm_Test_Lifetime_Experiment_16_06_2026_11_41_23_899.toml'
-lifetime_3P0_3H5 = 'Outputs/3P0-3H5_488.35nm_Lifetime_16_06_2026_14_03_58_850.toml'
-lifetime_3P0_3F3 = 'Outputs/3P0-3F3_488.35nm_Lifetime_16_06_2026_16_04_14_279.toml'
-lifetime_3P0_PC = 'Outputs/3P0_544.36_488.35_lifetime_scope_test_17_06_2026_16_28_03_077.toml'
-lifetime_3P0_PC_sparse = 'Outputs/3P0_544.36_488.35_lifetime_scope_test_sparse_17_06_2026_16_32_38_246.toml'
-life = 'Outputs/1D2-3H4_577.0_50ohm_lifetime_scope_605.33_30_06_2026_09_12_27_582.toml'
-life = 'Outputs/1D2-3H4_577.0_50ohm_lifetime_scope_611.93_30_06_2026_09_37_11_570.toml'
-life = 'Outputs/1D2-1G4_606.50_50ohm_lifetime_scope_30_06_2026_11_00_29_600.toml'
-life = 'Outputs/1D2-3H4_580.2_50ohm_lifetime_scope_13_07_2026_16_47_16_368.toml'
-
-data_path = Path(__file__).parent / life
-data = load_rex_data(data_path, "polars")
-
-waveforms = np.array(data['DPO7104_TekTronix_scope_waveform'][0])
-time_params = np.array(data['DPO7104_TekTronix_scope_time_from_trigger_parameters'][0])
-#spec_wavelength = np.array(data['iHR550_wavelength (nm)'][0])
-
-print(time_params)
-
-times = np.arange(0, time_params[0], step=time_params[1]) * time_params[2] + time_params[3] - time_params[4]
-fig, ax = plt.subplots(figsize=(14,8))
 def no_rise(v_all, t_all):
     #cut to find slope from trigger start
     #v_start = np.searchsorted(t_all, 0.0)
     v_all = -v_all
-    v_start = np.argmax(v_all) +0 #fit start at the clean top
+    v_start = np.argmax(v_all) -0 #fit start at the clean top
+    v_end = -1 #np.argmax(v_all)+30
     
-    v = v_all[v_start:-1]
-    t = t_all[v_start:-1]
+    v = v_all[v_start:v_end]
+    t = t_all[v_start:v_end]
     
     # 3. Provide initial guesses (Crucial for curve_fit)
     # Guess A as max voltage, tau as reasonable time, y0 as baseline/end value
@@ -63,13 +43,95 @@ def no_rise(v_all, t_all):
     ax.plot(t_all, v_all, 'k.')
     ax.vlines(t[0], min(v_all), max(v_all), linestyle='dashed', label='fit start')
     
-    ax.plot(t, v_fit, 'r', label=f'Tau={tau_fit:.5e} s')
+    ax.plot(t, v_fit, 'r', label=f'Tau={tau_fit:.3e} s')
     title = (f'Lifetime')
     ax.set_title(title, fontsize=20)
     plt.legend(loc='center right', fontsize=20)
     plt.tight_layout()
 
+def risetime(v_all, t_all):
+    v_all = -v_all
+    v_start = np.argmax(v_all) -27 #fit start at inital rise
+    v_end = -1 #np.argmax(v_all)+500
+    
+    v = v_all[v_start:v_end]
+    t = t_all[v_start:v_end]
+    
+    # 3. Provide initial guesses (Crucial for curve_fit)
+    # Guess A as max voltage, tau as reasonable time, y0 as baseline/end value
+    initial_guesses = [-max(v), (t[-1] - t[0])/100, max(v)/10, (t[-1] - t[0])/10, min(v)]
+    #initial_guesses = [-1.0e-2, 1.0e-5, 1.0e-4, 1.5e-4, min(v)]
+    print(initial_guesses)
+    
+    # 4. Fit the curve
+    popt, pcov = curve_fit(double_exponential, t, v, p0=initial_guesses)
+    print(popt)
+    
+    # Extract fitted parameters
+    A_fit, tau_fit, A2_fit, tau2_fit, y0_fit = popt
+    print(f"Fitted Rise Time (tau2): {tau2_fit:.6e} seconds")
+    
+    # 5. Calculate Fits and Residuals for plotting
+    v_fit = double_exponential(t, A_fit, tau_fit, A2_fit, tau2_fit, y0_fit)
+    residuals = v - v_fit
+    
+    # 6. Plotting
+    ax.set_xlabel('Time from Trigger (s)', fontsize=20, labelpad=3)
+    ax.set_ylabel('Voltage (V)', fontsize=20, labelpad=3)
+    ax.plot(t_all, v_all, 'k.')
+    ax.vlines(t[0], min(v_all), max(v_fit), linestyle='dashed', label='fit end')
+    
+    ax.plot(t, v_fit, 'r', label=f'Tau1={tau_fit:.3e} s\nTau2={tau2_fit:.3e} s')
+    title = (f'Rise Time')
+    ax.set_title(title, fontsize=20)
+    plt.legend(loc='center right', fontsize=20)
+    plt.tight_layout()
+
+
+odd_1D2_3H4_lifetime = 'Outputs/(3P0)1D2-3H4_488.35nm_Test_Lifetime_Experiment_16_06_2026_11_41_23_899.toml'
+lifetime_3P0_3H5 = 'Outputs/3P0-3H5_488.35nm_Lifetime_16_06_2026_14_03_58_850.toml'
+lifetime_3P0_3F3 = 'Outputs/3P0-3F3_488.35nm_Lifetime_16_06_2026_16_04_14_279.toml'
+lifetime_3P0_PC = 'Outputs/3P0_544.36_488.35_lifetime_scope_test_17_06_2026_16_28_03_077.toml'
+lifetime_3P0_PC_sparse = 'Outputs/3P0_544.36_488.35_lifetime_scope_test_sparse_17_06_2026_16_32_38_246.toml'
+#=======
+life3H4_site1 = 'Outputs/1D2-3H4_577.0_50ohm_lifetime_scope_605.33_30_06_2026_09_12_27_582.toml'
+life3H4_site1_other = 'Outputs/1D2-3H4_577.0_50ohm_lifetime_scope_611.93_30_06_2026_09_37_11_570.toml'
+life1G4 = 'Outputs/1D2-1G4_606.50_50ohm_lifetime_scope_30_06_2026_11_00_29_600.toml'
+life3H4_bigslit = 'Outputs/1D2-3H4_580.2_50ohm_lifetime_scope_13_07_2026_16_47_16_368.toml' #0.2 slits, need to redo for 0.1
+life3H5 = 'Outputs/1D2-3H5_606.35_50ohm_lifetime_scope_22_06_2026_12_33_45_333.toml'
+life3P0_1D2_3H4 = 'Outputs/(3P0)1D2-3H4_488.2_50ohm_lifetime_scope_14_07_2026_10_34_31_169.toml'
+life3P0_1D2_3H4_bigslits = 'Outputs/(3P0)1D2-3H4_488.2_50ohm_lifetime_scope_bigslits_14_07_2026_10_22_02_048.toml'
+life3P0_1D2_3H4_unknown = 'Outputs/(3P0)1D2-3H4_488.65_50ohm_lifetime_scope_14_07_2026_11_35_41_178.toml'
+life3P0_3H5 = 'Outputs/3P0-3H5_488.3_50ohm_lifetime_scope_14_07_2026_16_20_14_217.toml'
+life3P0_3H6_616 = 'Outputs/3P0-3H6_488.35_50ohm_lifetime_scope_15_07_2026_12_18_35_756.toml'
+life3P0_3H6_615 = 'Outputs/3P0-3H6_488.35_50ohm_lifetime_scope_615_15_07_2026_12_37_08_674.toml'
+life3P0_3H6_607 = 'Outputs/3P0-3H6_488.35_50ohm_lifetime_scope_607_15_07_2026_12_56_00_366.toml'
+life3P0_3H6_621 = 'Outputs/3P0-3H6_488.35_50ohm_lifetime_scope_621_15_07_2026_17_34_27_687.toml'
+life3P0_3F3 = 'Outputs/3P0-3F3_488.7_50ohm_lifetime_scope_21_07_2026_14_20_47_002.toml'
+
+life3P0_3F2_647 = 'Outputs/3P0-3F2_488.35_50ohm_lifetime_scope_647_15_07_2026_16_37_12_584.toml'
+life3P0_3F2_648 = 'Outputs/3P0-3F2_488.35_50ohm_lifetime_scope_648_15_07_2026_16_51_27_487.toml'
+life3P0_3F2_649 = 'Outputs/3P0-3F2_488.35_50ohm_lifetime_scope_649_15_07_2026_17_02_51_812.toml'
+life3P0_3F2_650 = 'Outputs/3P0-3F2_488.35_50ohm_lifetime_scope_650_15_07_2026_17_13_01_161.toml'
+life3P0_3F2_653 = 'Outputs/3P0-3F2_488.35_50ohm_lifetime_scope_653_15_07_2026_17_23_00_868.toml'
+life3P0_3F2_657 = 'Outputs/3P0-3F2_488.35_50ohm_lifetime_scope_657_15_07_2026_17_28_44_365.toml'
+
+data_path = Path(__file__).parent / life3P0_3F3
+title = '1D2-3H5 Lifetime'
+data = load_rex_data(data_path, "polars")
+
+waveforms = np.array(data['DPO7104_TekTronix_scope_waveform'][0])
+time_params = np.array(data['DPO7104_TekTronix_scope_time_from_trigger_parameters'][0])
+#spec_wavelength = np.array(data['iHR550_wavelength (nm)'][0])
+
+print(time_params)
+
+times = np.arange(0, time_params[0], step=time_params[1]) * time_params[2] + time_params[3] - time_params[4]
+fig, ax = plt.subplots(figsize=(9,5))
+
 no_rise(waveforms, times)
+#risetime(waveforms, times)
 
 #ax.plot(times, waveforms)
+plt.title(title)
 plt.show()
--- read_areas_rex_toml.py
+++ read_areas_rex_toml.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 from scipy import integrate, signal
+from scipy.interpolate import PchipInterpolator 
 
 from pathlib import Path
 from rex_utils import load_rex_data
@@ -33,6 +34,21 @@ sample3H4_580 = 'Outputs/1D2-3H4_580.2_emission_scope_amp_13_07_2026_12_03_47_48
 sample3H6_test = 'Outputs/1D2-3H6_606.45_emission_scope_ampTEST_13_07_2026_14_40_34_165.toml'
 sample3H6_full = 'Outputs/1D2-3H6_606.45_emission_scope_amp_13_07_2026_14_53_02_724.toml'
 sample3P03H4_4882 = 'Outputs/(3P0)1D2-3H4_488.2_emission_scopeTEST_14_07_2026_09_55_22_863.toml'
+sample3P03H4_4886 = 'Outputs/(3P0)1D2-3H4_488.65_emission_scopeTEST_14_07_2026_10_53_14_237.toml'
+sample3P03H4_4882 = 'Outputs/(3P0)1D2-3H4_488.25_emission_scope_14_07_2026_12_33_52_765.toml'
+sample3P03H5_test = 'Outputs/3P0-3H5_488.3_emission_scopeTEST_14_07_2026_14_23_30_846.toml'
+sample3P03H5 = 'Outputs/3P0-3H5_488.3_emission_scope_14_07_2026_14_40_44_075.toml'
+sample3P03H6_test = 'Outputs/3P0-3H6_488.35_emission_scope_ampTEST_15_07_2026_09_31_30_422.toml'
+sample3P03H6_test2 = 'Outputs/3P0-3H6_488.35_emission_scope_ampTEST2_15_07_2026_09_54_00_531.toml'
+sample3P03H6 = 'Outputs/3P0-3H6_488.35_emission_scope_amp_15_07_2026_10_12_10_468.toml'
+sample3P03F2 = 'Outputs/3P0-3F2_488.35_emission_scope_amp_15_07_2026_13_43_32_484.toml'
+sample3P03F3_test = 'Outputs/3P0-3F33F4_488.7_emission_scope_ampTEST_21_07_2026_09_48_14_527.toml'
+sample3P03F3_4884test = 'Outputs/3P0-3F33F4_488.4_emission_scope_ampTEST2_21_07_2026_10_17_40_310.toml'
+sample3P03F3 = 'Outputs/3P0-3F33F4_488.7_emission_scope_amp_21_07_2026_10_38_53_510.toml'
+sample3P03H6_dgates_test= 'Outputs/3P0-3H6_488.7_emission_scope_amp_dgatesTEST_21_07_2026_14_47_25_888.toml'
+sample3P03H6_dgates = 'Outputs/3P0-3H6_488.7_emission_scope_amp_dgates_21_07_2026_15_00_39_175.toml'
+sample3P03H5_test2 = 'Outputs/3P0-3H5_488.75_emission_scope_amp_longTEST_22_07_2026_09_10_14_338.toml'
+sample3P03H6_sgates = 'Outputs/3P0-3H6_488.7_emission_scope_amp_sgates_quick_21_07_2026_16_57_04_816.toml'
 
 def loader(filename):
 
@@ -54,15 +70,16 @@ def loader(filename):
 
 
 def plot_areas(wavenumbers, areas, name, ax, off=0.0, color='k', prominence=0.01, distance=20, y_offset_multiplier=1.0):
-        shifted_wavenumbers = np.ones_like(wavenumbers) *start_laser-wavenumbers+off
+        shifted_wavenumbers = np.ones_like(wavenumbers) *start_laser-wavenumbers+off #need to change this to do the offset on the wavelengths, then convert to wavenumbers
         norm_areas = (areas-min(areas))/max(areas) +(y_offset_multiplier-1.0)*y_offset
         ax.plot(shifted_wavenumbers, norm_areas, label=name, color=color)
+
         area_peaks, _ = signal.find_peaks(norm_areas, prominence=prominence, distance=distance) #adjust height as needed based on expected peak amplitudes
         for i in area_peaks:
                 ax.annotate(f'{shifted_wavenumbers[i]:.1f}', xy=(shifted_wavenumbers[i], norm_areas[i]), xytext=(5, 10), textcoords='offset points', color=color, fontsize=7)
 
 
-
+title = 'Emission Spectra'
 fig, ax = plt.subplots()
 start_laser = 16524 #the lowest top multiplet from the monitored transition, ideally the laser wavelength
 
@@ -70,49 +87,103 @@ start_laser = 16524 #the lowest top multiplet from the monitored transition, ide
 #shifted_wavenumbers = np.ones_like(wavenumbers) *start_laser-wavenumbers-0.0  #shift so the first point is at 0 cm^-1 for 3H4, adjust as needed based on expected peak positions
 #ax.plot(wavenumbers, areas, label='Spectra')
 
-# wavenumbers, areas = loader(sample3P03H4_4882)
-# ax.plot(wavenumbers, areas, label='3P0 Spectra')
+# wavenumbers, areas = loader(sample3P03H6)
+# areas = (areas-min(areas))/max(areas)
+# ax.plot(wavenumbers, areas, label='3P0-3H6 Spectra small gates')
 
 # wavenumbers, areas = loader(sample3H4_580)
-# ax.plot(wavenumbers, areas, label='580 Spectra')
+# areas = (areas-min(areas))/max(areas)
+# ax.plot(wavenumbers, areas, label='1D2-3H4 Spectra')
+
+# wavenumbers, areas = loader(sample3P03F2)
+# areas = (areas-min(areas))/max(areas)
+# ax.plot(wavenumbers, areas, label='3P0-3F2 Spectra')
+
+# wavenumbers, areas = loader(Indirect_3H4)
+# areas = (areas-min(areas))/max(areas)
+# ax.plot(wavenumbers+65, areas, label='Test(3P0)1D2-3H4 Spectra')
+
+# wavenumbers, areas = loader(sample3P03H4_4882)
+# areas = (areas-min(areas))/max(areas)
+# ax.plot(wavenumbers, areas, label='(3P0)1D2-3H4 Spectra')
+
+# jon_data = np.loadtxt('Outputs/598nm-683nm 0.1nm step P0 to H6.dat', skiprows=1)
+# wavelnumbers_jon = 1e7/(jon_data[:,0][-500:-1])
+# areas_jon = jon_data[:,1][-500:-1]
+# areas_jon = (areas_jon-min(areas_jon))/max(areas_jon)
+# ax.plot(wavelnumbers_jon+10, areas_jon, label='Jon\'s Data 3P0-3F2')
+
+wavenumbers1, areas1 = loader(sample3P03H6)
+#areas = (areas-min(areas))/max(areas)
+ax.plot(wavenumbers1, areas1*1.45, label='Old Spectra, intensity multiplied by 1.45', linestyle='dotted', color='k')
+
+wavenumbers2, areas2 = loader(sample3P03H6_dgates)
+#areas = (areas-min(areas))/max(areas)
+ax.plot(wavenumbers2, areas2, label='34us time gates Spectra')
+
+wavenumbers3, areas3 = loader(sample3P03H6_sgates)
+#areas = (areas-min(areas))/max(areas)
+ax.plot(wavenumbers3, areas3, label='17us time gates Spectra (0.08 stepsize)')
+
+areas3_resized = PchipInterpolator(1e7/wavenumbers3, areas3)(1e7/wavenumbers2)
+endclip = 100
+ax.plot(wavenumbers2[:-endclip], areas3_resized[:-endclip], label='17us time gates Spectra resized', linestyle='dashed')
+
+fixed_areas = areas3_resized*2 - areas2 #Not sure if this is the way Jon meant
+ax.plot(wavenumbers2[:-endclip], fixed_areas[:-endclip], label='Fixed Spectra with 1D2 subtracted out')
+
+fixed_areas2 = areas1*1.45*2 - areas2 #Not sure if this is the way Jon meant
+ax.plot(wavenumbers2[:-endclip], fixed_areas2[:-endclip], label='Fixed Spectra with 1D2 subtracted out (Old Spectra)')
+
+title = '3P0-3H6 Emission Spectra with Different Time Gates'
+
+# wavenumbers, areas = loader(sample3P03H5)
+# #areas = (areas-min(areas))/max(areas)
+# ax.plot(wavenumbers, areas, label='Spectra')
+
+# wavenumbers, areas = loader(sample3P03H5_test2)
+# #areas = (areas-min(areas))/max(areas)
+# ax.plot(wavenumbers, areas, label='Spectra')
+
+ax.invert_xaxis()
+
+
+# y_offset = 0.05
+# off1 = 72.15
+# off2 = -2.17
+# off3 = 58.65
+
+# wavenumbers, areas = loader(sample3P03H4_4882)
+# step_size = np.mean(np.diff(1e7/wavenumbers))
+# name = f'Indirect 488.20 (3P0) Data\n{step_size:.2f} step, 7 avg, 0.1 slits'
+# color = 'orange'
+# plot_areas(wavenumbers, areas, name, ax, off=off1, color=color, prominence=0.01, distance=20, y_offset_multiplier=2.0)
+
+# # wavenumbers, areas = loader(sample3P03H4_4886)
+# # step_size = np.mean(np.diff(1e7/wavenumbers))
+# # name = f'Indirect 488.65(3P0) Data\n{step_size:.2f} step, 2 avg, 0.1 slits'
+# # color = 'hotpink'
+# # plot_areas(wavenumbers, areas, name, ax, off=off1+0.54, color=color, prominence=0.01, distance=20, y_offset_multiplier=3.0)
 
 # wavenumbers, areas = loader(Direct_3H4)
-# ax.plot(wavenumbers, areas, label='576 Spectra')
+# step_size = np.mean(np.diff(1e7/wavenumbers))
+# name = f'Site 1 Direct 576.90 (1D2) Data\n{step_size:.2f} step, 4 avg, 0.1 slits, amplified'
+# color = 'r'
+# plot_areas(wavenumbers, areas, name, ax, off=off2, color=color, prominence=0.01, distance=20, y_offset_multiplier=4.0)
 
-# ax.invert_xaxis()
+# wavenumbers, areas = loader(sample3H4_580)
+# step_size = np.mean(np.diff(1e7/wavenumbers))
+# name = f'Direct 580.00 (1D2) Data\n{step_size:.2f} step, 8 avg, 0.1 slits, amplified'
+# color = 'b'
+# plot_areas(wavenumbers, areas, name, ax, off=70.5, color=color, prominence=0.01, distance=20, y_offset_multiplier=3.0)
 
+# jon_data = np.loadtxt('Outputs/600nm-650nm 0.01nm step D2 to H4 (2).dat', skiprows=1)
+# wavelnumbers_jon = 1e7/jon_data[:,0]
+# areas_jon = jon_data[:,1]
+# name = 'Jon\'s Data'
+# color = 'g'
+# plot_areas(wavelnumbers_jon, areas_jon, name, ax, off=off3, color=color, prominence=0.01, distance=20, y_offset_multiplier=1.0)
 
-y_offset = 0.1
-off1 = 72.7
-off2 = -2.17
-off3 = 58.65
-
-wavenumbers, areas = loader(sample3P03H4_4882)
-step_size = np.mean(np.diff(1e7/wavenumbers))
-name = f'Indirect (3P0) Data\n{step_size:.2f} step, 15 avg, 0.2 slits'
-color = 'm'
-plot_areas(wavenumbers, areas, name, ax, off=off1, color=color, prominence=0.01, distance=20, y_offset_multiplier=2.0)
-
-wavenumbers, areas = loader(Direct_3H4)
-step_size = np.mean(np.diff(1e7/wavenumbers))
-name = f'Direct 576 (1D2) Data\n{step_size:.2f} step, 4 avg, 0.1 slits, amplified'
-color = 'r'
-plot_areas(wavenumbers, areas, name, ax, off=off2, color=color, prominence=0.01, distance=20, y_offset_multiplier=3.0)
-
-wavenumbers, areas = loader(sample3H4_580)
-step_size = np.mean(np.diff(1e7/wavenumbers))
-name = f'Direct 580 (1D2) Data\n{step_size:.2f} step, 8 avg, 0.1 slits, amplified'
-color = 'b'
-plot_areas(wavenumbers, areas, name, ax, off=70.5, color=color, prominence=0.01, distance=20, y_offset_multiplier=4.0)
-
-jon_data = np.loadtxt('Outputs/600nm-650nm 0.01nm step D2 to H4 (2).dat', skiprows=1)
-wavelnumbers_jon = 1e7/jon_data[:,0]
-areas_jon = jon_data[:,1]
-name = 'Jon\'s Data'
-color = 'g'
-plot_areas(wavelnumbers_jon, areas_jon, name, ax, off=off3, color=color, prominence=0.01, distance=20, y_offset_multiplier=1.0)
-
-ax.xaxis.set_minor_locator(ticker.AutoMinorLocator())
 
 # area_peaks, _ = signal.find_peaks(norm_areas, height=0.00001, distance=80) #adjust height as needed based on expected peak amplitudes
 # ax.plot(shifted_wavenumbers[area_peaks], norm_areas[area_peaks], 'rx', label='Peaks')
@@ -132,9 +203,12 @@ ax.set_xlabel('Wavenumber (cm$^{-1}$)')
 secax = ax.secondary_xaxis('top', functions=(cmnminvert, cmnminvert))  #to put wavelength on top
 secax.set_xlabel('Wavelength (nm)')
 secax.tick_params(axis = 'x')
+secax.xaxis.set_minor_locator(ticker.AutoMinorLocator())
+ax.grid()
+ax.xaxis.set_minor_locator(ticker.AutoMinorLocator())
 ax.set_ylabel('Intensity')
 ax.tick_params(axis = 'x')
 ax.tick_params(axis = 'y')
-ax.set_title('Emission Spectra')
+ax.set_title(title)
 plt.legend()
 plt.show()
